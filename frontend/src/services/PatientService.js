96102c7b3b08c8206bed630faabf64dbbe4d6724
uzieltzab <uzieltzab8@gmail.com>
2025-12-13 01:50:05 -0600

    Initial frontend patient form

---
+++ frontend/src/services/PatientService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8001/api';
+
+export default {
+    
+    getPatients() {
+        return axios.get(`${API_URL}/patients`);
+    },
+
+    getPatient(id) {
+        return axios.get(`${API_URL}/patients/${id}`);
+    },
+
+    createPatient(data) {
+        return axios.post(`${API_URL}/patients`, data);
+    },
+
+    updatePatient(id, data) {
+        return axios.put(`${API_URL}/patients/${id}`, data);
+    },
+
+    deletePatient(id) {
+        return axios.delete(`${API_URL}/patients/${id}`);
+    },
+
+    getHospitals() {
+        return axios.get(`${API_URL}/hospitals`);
+    },
+
+    getTutors() {
+        return axios.get(`${API_URL}/tutors`);
+    }
+};
